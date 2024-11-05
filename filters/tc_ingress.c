14b80861b15019058f919d1718a01135ce4437b7
Memet Bilgin <memetb@gmail.com>
2024-11-05 05:03:40 -0400

    refactor: remove dead code and use common definitions

--- filters/tc_ingress.c
+++ filters/tc_ingress.c
@@ -6,6 +6,8 @@
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
+#include "common.h"
+
 //
 //                  --- [tap0] -- [nic0]
 //                 /
@@ -27,65 +29,13 @@ struct {
 
 #define DEDUP_WINDOW_NS 1500000000  // 1.5s // TODO: MAKE THIS CONFIGURABLE
 
-static __always_inline int strip_header(struct __sk_buff *skb, __u32 size, __be16 ether_type) {
-    // Clone the packet with redirection, creating a writable clone
-    /*
-    int new_skb = bpf_clone_redirect(skb, skb->ifindex, 0);
-    if (new_skb < 0) {
-        bpf_printk("Clone failed");
-        return TC_ACT_SHOT;  // Drop the packet if cloning fails
-    }
-
-    struct ethhdr header_data;
-    if (bpf_skb_load_bytes(new_skb, 0, &header_data, sizeof(header_data)) < 0) {
-        // load the ethernet header
-        return TC_ACT_SHOT;
-    }
-
-    if (eth->h_proto != bpf_htons(ETH_P_802_EX1)){
-        bpf_printk("ether protocol mismatch - ignoring (0x%x != 0x%x)", eth->h_proto, bpf_htons(ETH_P_802_EX1));
-        return TC_ACT_OK;
-    }
-
-    header_data.h_proto = ether_type;
-
-
-    // Adjust the cloned packet to remove the first `size` bytes
-    if (bpf_skb_adjust_room(skb, -size, BPF_ADJ_ROOM_MAC, 0) < 0) {
-        bpf_printk("Failed to adjust room");
-        return TC_ACT_SHOT;  // Drop if unable to adjust
-    }
-
-    // Re-fetch data pointers after adjustment
-    void *data = (void *)(long)skb->data;
-    void *data_end = (void *)(long)skb->data_end;
-
-    // Ensure there's enough room for the remaining MAC header
-    struct ethhdr *eth = data;
-    if ((void *)(eth + 1) > data_end) {
-        return TC_ACT_SHOT;  // Drop if packet is too short
-    }
-
-    if (bpf_skb_store_bytes(skb, 0, &header_data, sizeof(header_data), 0) < 0) {
-        // load the ethernet header
-        bpf_printk("Failed to update ethernet header on new frame");
-        return TC_ACT_SHOT;
-    }
-    */
-    // Return success to indicate packet is ready to be sent
-    return TC_ACT_SHOT;
-}
-
 SEC("tc")
 int ingress_deduplicate(struct __sk_buff *skb)
 {
-    if (skb->mark != 0xCAFE) {
+    if (skb->mark != mark) {
         // packet is not coming to us from our slave interfaces, it is coming from
         // the "outside". Do not strip or do anything.
-        bpf_printk("non marked packet - this is coming from the outside");
-        //return TC_ACT_OK;
-    } else {
-        bpf_printk("packet is marked - this is coming from the slave interface");
+        return TC_ACT_OK;
     }
 
 		// Access packet data
@@ -93,58 +43,36 @@ int ingress_deduplicate(struct __sk_buff *skb)
 		void *data_end = (void *)(long)skb->data_end;
     int len = data_end - data;
 
+    struct custom_header header;
     // Ensure we have enough room for the Ethernet header + counter
     struct ethhdr *eth = data;
-    if ((void *)(eth + 1) + 4 > data_end) {
+    if ((void *)(eth + 1) + sizeof(header.data.sequence) > data_end) {
         return TC_ACT_SHOT;  // Drop if packet is too short
     }
 
-    /*
-    if (eth->h_proto != bpf_htons(ETH_P_802_EX1)){
-        bpf_printk("ether protocol mismatch - ignoring (0x%x != 0x%x)",
-                   eth->h_proto, bpf_htons(ETH_P_802_EX1));
-        return TC_ACT_OK;
-    }
-    */
-
     // get the sequence number
-    __u16 seq_num = 0;
-    if (bpf_skb_load_bytes(skb, len - 4, &seq_num, sizeof(seq_num)) < 0) {
+    if (bpf_skb_load_bytes(skb, len - sizeof(header.data.sequence), &header.data.sequence, sizeof(header.data.sequence)) < 0) {
         bpf_printk("Can't load sequence");
         return TC_ACT_SHOT;
     }
 
-    // get the original ethernet frame type
-    __u16 ether_type = 0;
-    if (bpf_skb_load_bytes(skb, len - 2, &ether_type, sizeof(ether_type)) < 0) {
-        bpf_printk("Can't load old type");
-        return TC_ACT_SHOT;
-    }
-
-    int eth_type_offset = offsetof(struct ethhdr, h_proto);
-    // Use bpf_skb_store_bytes to modify the eth_type
-    if (bpf_skb_store_bytes(skb, eth_type_offset, &ether_type, sizeof(ether_type), 0 ) < 0){
-        bpf_printk("failed to update protocol");
-        return TC_ACT_OK;
-    }
-
-    bpf_printk("Packet sequence and protocol are: %d 0x%x", seq_num, ether_type);
+    bpf_printk("Packet sequence number and protocol are: %d 0x%x", header.data.sequence, eth->h_proto);
 
     // Get current timestamp
     __u64 now = bpf_ktime_get_ns();
 
     // Look up in map
-    __u64 *seen = bpf_map_lookup_elem(&seen_packets, &seq_num);
+    __u64 *seen = bpf_map_lookup_elem(&seen_packets, &header.data.sequence);
     if (seen){
         // Check if within dedup window
         if (now - *seen < DEDUP_WINDOW_NS) {
-            bpf_printk("Dropped duplicate packet (serial: %d)", seq_num);
+            bpf_printk("Dropped duplicate packet (serial: %d)", header.data.sequence);
             return TC_ACT_SHOT;  // Drop duplicate packet
         }
     }
 
     // Not a duplicate, add to map
-    bpf_map_update_elem(&seen_packets, &seq_num, &now, BPF_ANY);
+    bpf_map_update_elem(&seen_packets, &header.data.sequence, &now, BPF_ANY);
 
     return TC_ACT_OK;
 }
